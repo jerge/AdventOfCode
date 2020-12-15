c801d1f7bcc921732b5e11a16aecb77574bcfd8a
jerge <erikjer@student.chalmers.se>
2020-12-15 14:37:16 +0100

    Day 15

---
+++ 2020/aoc15.1.hs
@@ -0,0 +1,30 @@
+import qualified Data.Map as Map
+import qualified Data.Text as T
+
+main = do 
+  x <- readFile "./15"
+  let a = zip (map (readInt . T.unpack) $ T.split (==',') (T.pack x)) [1..]
+  let b = Map.fromList $ a
+  print b
+  let (l,t) = last a
+  speak l t b
+
+readInt :: String -> Int
+readInt s = read s :: Int
+
+speak :: Int -> Int -> Map.Map Int Int -> IO Int
+speak l 2020 _ = do 
+    return l
+speak l t m = case Map.lookup l m of
+    Just x -> do
+        let n = t-x
+        let m' = Map.insert l (t) m
+        --print (t+1,n)
+        --print m'
+        speak n (t+1) m'
+    Nothing -> do
+        let n = 0
+        let m' = Map.insert l (t) m
+        --print (t+1,n)
+        --print m'
+        speak n (t+1) m'
